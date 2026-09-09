f0013ed8e4f957ec289c168c1970fcebb02c5f33
vronooa <122388917+hbsu@users.noreply.github.com>
2026-09-08 22:04:40 -0400

    Add: reverse-bits - submission-7

---
+++ Data Structures & Algorithms/reverse-bits/submission-7.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int result = 0;
+        for(int i = 0; i < 32; i++){
+            result <<= 1;
+            if(n & (1 << i)) result += 1;
+
+
+        }
+        return result;
+    }
+};
